b48d1bc10c3baabcce46912cbc2a847a41f7abfe
barale2906 <alexanderbarajas@gmail.com>
2024-07-13 22:22:13 -0500

    Creación de plantilla base, barra de navegación

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
-            $table->integer('rol_id');
+            $table->boolean('status')->default(true)->comment('false Saldo Inactivo, true Saldo Activo');
+            $table->integer('rol_id')->comment('1 Superusuario, 2 Administrador, 3 Coordinador, 4 Auxiliar');
             $table->timestamps();
         });
     }
